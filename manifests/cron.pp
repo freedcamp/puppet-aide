cc65e2bcbf28fc2d047541350787a5f59a1e4712
Vadym Chepkov <vchepkov@gmail.com>
2016-10-18 07:57:31 -0400

    suppress OK report

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,10 +1,20 @@
 # Class for managing aide's cron job.
-class aide::cron inherits aide {
+class aide::cron (
+  $aide_path = $aide::params::aide_path,
+  $mailto    = 'root',
+) inherits aide {
+  file { '/usr/sbin/aide-check':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0744',
+    content => template("${module_name}/aide-check.erb"),
+  }
   cron { 'aide':
-    command => "${::aide::params::aide_path} --check",
+    command => '/usr/sbin/aide-check',
     user    => 'root',
     hour    => '*',
-    minute  => $::aide::minute,
-    require => [Package['aide'], Exec['install aide db']]
+    minute  => $minute,
+    require => [Package['aide'], Exec['install aide db'], File['/usr/sbin/aide-check']],
   }
 }
